5530d4c613763269e93ee8706b2082a9911ca9e6
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-01 10:03:08 -0600

    fix: Finalizando el drawer basico

--- src/navigation/MenuLateralBasico.tsx
+++ src/navigation/MenuLateralBasico.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import {StackNavigator} from "./StackNavigator";
 import {SettingsScreen} from "../screens";
+import {useWindowDimensions} from "react-native";
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
+  const {width} = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerType: width >= 768 ? "permanent" : "front", // Menú modo horizontal
+        // headerShown: false, // Oculta la hamburguesa
+      }}>
       <Drawer.Screen name="SatckNavigator" component={StackNavigator} />
       <Drawer.Screen name="SettingScreen" component={SettingsScreen} />
     </Drawer.Navigator>
