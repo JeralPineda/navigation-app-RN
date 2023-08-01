233a8c1ec45c0d4b6b40349776c5d8a6b9eb76e4
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-01 08:29:08 -0600

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: "@react-native",
+  rules: {
+    "prettier/prettier": 0,
+    quotes: [2, "double"],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {Text} from "react-native";
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Text>App</Text>
+    </NavigationContainer>
+  );
+}
+
+export default App;
