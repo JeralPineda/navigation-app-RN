5ff596fa32e8466ee7022b13228eefaa13d89abf
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-02 14:10:17 -0600

    feat: disparar el SignIn desde otra pantalla

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import {Text, View} from "react-native";
+import React, {useContext} from "react";
+import {Button, Text, View} from "react-native";
+
+import {styles} from "../theme/theme";
+import {AuthContext} from "../context/AuthContext";
 
 export const ContactsScreen = () => {
+  const {
+    signIn,
+    authState: {isLoggedIn},
+  } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>Contacts</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Contacts</Text>
+
+      {!isLoggedIn && <Button title="SignIn" onPress={signIn} />}
     </View>
   );
 };
