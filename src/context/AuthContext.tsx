44b0e62490fbc1dc276a3394471c1a904cb4ca40
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-02 13:41:48 -0600

    feat: creando useReducer

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, {createContext} from "react";
+import React, {createContext, useReducer} from "react";
+import {authReducer} from "./authReducer";
 
 //Definir como luce, que información tendré aquí
 export interface AuthState {
@@ -25,10 +26,12 @@ export const AuthContext = createContext({} as AuthContextProps);
 
 // Componente proveedor del estado
 export const AuthProvider = ({children}: any) => {
+  const [authState, dispatch] = useReducer(authReducer, authInitialState);
+
   return (
     <AuthContext.Provider
       value={{
-        authState: authInitialState,
+        authState,
         signIn: () => {},
       }}>
       {children}
